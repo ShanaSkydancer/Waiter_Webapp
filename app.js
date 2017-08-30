79b97443199a0512cafd77b58c64e17398fb7f70
ShanaSkydancer <shana.proverbs31@gmail.com>
2017-08-30 14:57:19 +0200

    Added layout

---
+++ app.js
@@ -0,0 +1,99 @@
+'use strict';
+
+const express = require('express');
+const flash = require('express-flash');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const $ = require("jquery");
+const format = require("util").format;
+
+var app = express();
+
+//Folders being accessed
+//'public' is the folder that styling, fonts, images are in
+app.use(express.static('public'));
+//'views' is the folder where my layouts are in
+app.use(express.static('views'));
+//'routes' is where my specific handlebar templates and js functions are
+app.use(express.static('routes'));
+//'vendors' is where my template files are
+app.use(express.static('vendors'));
+
+//Connect Mongoose to your .js and have it access the MongoBD
+// const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/waiter_webapp";
+// mongoose.connect(mongoURL);
+
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', () => {
+  //   console.log('Connected to the DB!');
+  // });
+  
+//   var regiSchema = mongoose.Schema({
+//     regiNum: String
+//   });
+//   const regiNumModel = mongoose.model('regiNumModel', regiSchema);
+//Port and environment variable
+app.set('port', (process.env.PORT || 3000));
+
+//Use ExpressHandlebars
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+//Use bodyParser
+var jsonParser = bodyParser.json();
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+//Use flash for error messages
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+app.use(flash());
+
+//Functions being accessed
+const WaiterRoutes = require("./waiteravailability");
+
+//Access the function
+const waiterAvailabilityRoutes = WaiterRoutes();
+
+//Using "/" makes it the "index page" i.e. it has no route
+app.get('/', (req, res) => {
+    res.render("index");
+});
+
+//Showing the loging
+// app.get('/waiter', waiterAvailabilityRoutes.login);
+//Route to add waiters
+// app.get('/waiter/add', waiterAvailabilityRoutes.add);
+app.get('/log-in', function (req, res) {
+  res.render("login");
+});
+
+app.get('/admin', function (req, res) {
+  res.render("admin");
+});
+
+app.get('/waiter', function (req, res) {
+  res.render("waiter");
+});
+
+
+//Post data
+// app.post('/waiter/add', waiterAvailabilityRoutes.add);
+
+//Hosts my server
+  var server = app.listen(app.get("port"), () => {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Registration Numbers Webapp listening at http://%s:%s', host, port);
+});
---
+++ waiteravailability.js
@@ -0,0 +1,29 @@
+module.exports = () => {
+'use strict';
+
+//Use debugger with node-inspector
+//Used it at the beginning of my code but it can go anywhere or I can create a break in the inspector
+debugger;
+
+
+
+//     const login = (req, res) => {
+       
+//     };
+
+//     const index = (req, res) => {
+       
+//     };
+
+//     const add = (req, res, next) => {
+//     };
+
+//     const filter = (req, res) => {
+//     };
+
+//     return {
+//         index,
+//         add,
+//         fliter
+//     }
+};
